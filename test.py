383645d519a1319d71a1e575fd278276c6f3d919
zhangji <chenzhangji@gmail.com>
2020-12-31 11:23:31 -0600

    test

--- test.py
+++ test.py
@@ -1,17 +1,20 @@
 from gpiozero import PWMOutputDevice
 from time import sleep
+from signal import pause
 
 
 
 def run():
     led = PWMOutputDevice(4)
-    while True:
-        led.value = 0  # off
-        sleep(1)
-        led.value = 0.5  # half brightness
-        sleep(1)
-        led.value = 1  # full brightness
-        sleep(1)
+    led.value = 0.5
+    # while True:
+    #     led.value = 0  # off
+    #     sleep(1)
+    #     led.value = 0.5  # half brightness
+    #     sleep(1)
+    #     led.value = 1  # full brightness
+    #     sleep(1)
 
 if __name__ == "__main__":
     run()
+    pause()
